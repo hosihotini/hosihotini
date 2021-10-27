0c034a7dc0491c9406a8b3abc5598346ae3d735c
hosihotini <hosihotini0801@gmail.com>
2021-10-27 10:48:08 +0700

    TryDay1

---
+++ resources/views/login.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+
+        <!-- style -->
+        <style>
+		body {
+			background-image: url(https://www.ngobrolin.id/wp-content/uploads/2019/06/Di-Balik-Layar-Animasinopal-Kartun-Asli-Indonesia-758x505.jpg);
+			background-size: cover;
+			background position: center;
+			background-attachment: fixed;
+		}
+		.btn{
+			border: 1px solid;
+			padding: 5px 30px;
+			color: black;
+			font-family: sans-serif;
+			transition: 0,6s;
+			border-radius: 5px;
+			margin: 15px;
+		}
+		</style>
+    </head>
+   
+    <div class="container mt-4">
+
+    </div>
+    <center>
+    <h1>Membuat Login | hossi.com</h1>
+	<h3>Halaman Login Sederhana</h3>
+	<form action="login.php" method="post">		
+		<table>
+			<tr>
+				<td>Username</td>
+				<td><input type="text" name="username"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><a href="https://github.com/" class="btn">Log in</a></td>
+	</tr>
+			</table>
+	</form>
+    </center.
+        </div>     
+    </body>
+</html>
+
+
+
+
+
